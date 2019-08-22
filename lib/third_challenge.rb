b859abc695abb0c14201f8f180ac2e0ee80e0574
virajransing <virajransing@gmail.com>
2019-08-22 18:14:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,12 +13,24 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :name => "Lord Capulet",
+        :age => "50"
+        },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
+        },
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+        },
       :heroine_friends => []
    }
   }
+
+  
+end
 
   
 end
