4699b3629e43e02ad38d6b6ec13fad917cdaefe6
virajransing <virajransing@gmail.com>
2019-08-22 18:13:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,12 +3,12 @@ def second_challenge
     :montague => {:patriarch => {}, 
                   :matriarch => {}, 
                   :hero => {}, 
-                  :hero_friends => {}, 
+                  :hero_friends => [], 
                   }, 
     :capulet =>  {:patriarch => {},
                   :matriarch => {} ,
                   :heroine => {},
-                  :heroine_friends => {},
+                  :heroine_friends => [],
                    }
     
   }
