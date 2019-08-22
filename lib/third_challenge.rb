533f37ddd864279ac728be41da8096873bd73829
virajransing <virajransing@gmail.com>
2019-08-22 18:12:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,10 +4,10 @@ def third_challenge
       :patriarch => {:name => "Lord Montague", :age ="53"
         },
       :matriarch => {
-        :name => ""
+        :name => "Lady Montague", :age = "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
    }, 
